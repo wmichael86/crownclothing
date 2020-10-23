eef8278aed3b63f769eaed258a279db686d10a99
Michael Lewis <wmichael.lewis@outlook.com>
2020-10-23 01:59:17 -0700

    adding content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
 import './App.css';
+
+import HomePage from './Components/homepage/homepage'
 
 function App() {
   return (
-    <div >Alright Alright Alright</div>
+    <div >
+    <HomePage />
+    </div>
   );
 }
 
